55c7757eb2e0f9aa1d448762f687a6950e6a277e
yassine-eb <yassineelbouamraoui33@gmail.com>
2026-05-08 16:09:41 +0200

    fix: use psycopg3 compatible with Python 3.13

--- backend/bricoleup/settings.py
+++ backend/bricoleup/settings.py
@@ -127,7 +127,7 @@ if DATABASE_URL:
         'default': dj_database_url.config(
             default=DATABASE_URL,
             conn_max_age=600,
-            ssl_require=True,
+            engine='django.db.backends.postgresql',
         )
     }
 else:
