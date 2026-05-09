bf38eca8150b4d7a410fa54b34362c2b97b70d33
yassine-eb <yassineelbouamraoui33@gmail.com>
2026-05-09 16:29:22 +0200

    revert: restauration version avant Railway

--- backend/bricoleup/settings.py
+++ backend/bricoleup/settings.py
@@ -123,8 +123,13 @@ WSGI_APPLICATION = 'bricoleup.wsgi.application'
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
 else:
     DATABASES = {
         'default': {
